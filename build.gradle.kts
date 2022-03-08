1bb90adf2f3cbc384f91d522327244f58f85a1cf
Oguz Kocer <oguz.kocer@automattic.com>
2022-03-08 15:40:53 -0500

    Setup react-native-get-random-values and force react-native version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.github.node-gradle.node") version "3.1.1"
+    id("com.android.library") apply false
 }
 
 node {
@@ -7,3 +8,39 @@ node {
     version.set("16.14.0")
 }
 
+val reactNativeVersion = "0.66.4"
+val defaultCompileSdkVersion = 30
+val defaultMinSdkVersion = 21
+val defaultTargetSdkVersion = 30
+
+// Set project extra properties
+project.ext.set("compileSdkVersion", defaultCompileSdkVersion)
+project.ext.set("minSdkVersion", defaultMinSdkVersion)
+project.ext.set("targetSdkVersion", defaultTargetSdkVersion)
+
+allprojects {
+    repositories {
+        exclusiveContent {
+            forRepository {
+                maven {
+                    url = uri("https://a8c-libs.s3.amazonaws.com/android/react-native-mirror")
+                }
+            }
+            filter {
+                includeModule("com.facebook.react", "react-native")
+            }
+        }
+
+        mavenCentral()
+        google()
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            dependencySubstitution {
+                substitute(module("com.facebook.react:react-native"))
+                    .with(module("com.facebook.react:react-native:$reactNativeVersion"))
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+pluginManagement {
+    plugins {
+        id("com.android.library").version("4.2.2")
+    }
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+}
 
+include(":react-native-get-random-values")
+project(":react-native-get-random-values").projectDir = File(rootProject.projectDir, "node_modules/react-native-get-random-values/android")
