cafc6cce46f15912d2e264d9d974ef1d1cf4526f
Carlos Garcia <fluiddot@gmail.com>
2022-03-09 17:27:15 +0100

    Add react-native-reanimated package

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,6 @@ pluginManagement {
 
 include(":react-native-get-random-values")
 project(":react-native-get-random-values").projectDir = File(rootProject.projectDir, "node_modules/react-native-get-random-values/android")
+
+include(":react-native-reanimated")
+project(":react-native-reanimated").projectDir = File(rootProject.projectDir, "node_modules/react-native-reanimated/android")
