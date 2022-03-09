7d641ef43fe5e09c3c94499007e9d209b711d8ae
Carlos Garcia <fluiddot@gmail.com>
2022-03-09 14:02:21 +0100

    Add react-native-safe-area-context package

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,6 @@ pluginManagement {
 
 include(":react-native-get-random-values")
 project(":react-native-get-random-values").projectDir = File(rootProject.projectDir, "node_modules/react-native-get-random-values/android")
+
+include(":react-native-safe-area-context")
+project(":react-native-safe-area-context").projectDir = File(rootProject.projectDir, "node_modules/react-native-safe-area-context/android")
